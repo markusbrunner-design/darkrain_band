9051b0b616dccc6dfa158a68969332b3f1d36837
Markus Brunner <mbrunner@united-internet.de>
2025-08-27 12:27:15 +0200

    changes

--- shop/bilder_gross.php
+++
@@ -1,7 +0,0 @@
-<?php include '../include/head.php'; ?>
-
-<img src="bilder/<?php echo $_GET['bild']; ?>" alt="<?php echo $_GET['bild']; ?>"/>
-<p class="inhalt1">schlie&szlig;t dieses Fenster, um zum Shop zur&uuml;ckzukehren...</p>
-
-</body>
-</html>
