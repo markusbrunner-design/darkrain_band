398584d1637a978b9bb23ff7581bbc283ac207c2
Markus Brunner <mbrunner@united-internet.de>
2025-08-27 12:00:18 +0200

    changes

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const domain = document.querySelector('meta[name="DR.domain"]').getAttribute('content');
+
+document.querySelectorAll('.ajax-link').forEach(link => {
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		const link = this.getAttribute('href');
+        const absoluteLink = [domain, link].join('');
+
+        // gets the html content of the absolute link and puts it into the <div id="content"></div>
+        fetch(absoluteLink)
+          .then(response => response.text())
+          .then(html => {
+            document.querySelector('#content').innerHTML = html;
+          })
+          .catch(error => {
+            console.error('Error fetching content:', error);
+          });
+	});
+});
